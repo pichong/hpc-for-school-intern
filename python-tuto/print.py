7a87ef840f71e1a3840eb8213a9b0ae9fd355780
Gregoire Pichon <gregoire.pichon@atos.net>
2023-12-06 13:41:36 +0100

    python-tuto: print function

---
+++ python-tuto/print.py
@@ -0,0 +1,16 @@
+# print a text
+print("First message")
+
+# print a text and a variable value
+val=5
+print("Val is: ", val)
+
+# print a text including several variable values
+a="Monday"
+b=12
+c="December"
+print("Today, it's {} the {}th of {}".format(a, b, c))
+
+# print a decimal value with a specified format
+r=1/3
+print("1/3 is almost equal to {:.2f}".format(r))
